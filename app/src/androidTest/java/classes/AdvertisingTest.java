df66a8dc041806276fb0f5dc3bb8f3cd9e516af8
TaizaFrosino <carolfrosino@hotmail.com>
2018-06-04 01:07:28 -0300

    TestUnit

---
+++ app/src/androidTest/java/classes/AdvertisingTest.java
@@ -0,0 +1,40 @@
+package classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdvertisingTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void setTitle() {
+    }
+
+    @Test
+    public void getCategory() {
+    }
+
+    @Test
+    public void setCategory() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getThumbnail() {
+    }
+
+    @Test
+    public void setThumbnail() {
+    }
+}
